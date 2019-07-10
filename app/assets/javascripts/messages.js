7234fa5141817f80bb96eb6b7bc519c603c3d608
kaneko-ryota <ryota1611292@icloud.com>
2019-07-10 17:21:41 +0900

    update

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,16 +1,48 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+
+  function buildHTML(message){
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                       ${ message.name}
+                    </div>
+                    <div class="upper-message__date">
+                       ${message.date }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                      ${message.body}
+                  </div>
+                  ${img}
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "post",
-        data: formData,
-        dataType: 'json',
-        procassData: false,
-        contentType: false
-      })
-  })
-})
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      // $('.textbox').val('')
+      $("#new_message")[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $(".form__submit").attr("disabled", false)
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
+      $(".form__submit").prop("disabled", false)
+    })
+  });
+});
 
