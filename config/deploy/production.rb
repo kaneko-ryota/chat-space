edc02a4c42a46e747a6bf561823d39be00366794
kaneko-ryota <ryota1611292@icloud.com>
2019-07-13 20:25:28 +0900

    高額請求

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 server '52.194.41.185', user: 'ec2-user', roles: %w{app db web}
 
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
